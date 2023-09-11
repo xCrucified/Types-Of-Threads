6524e66a5dc4340f8ee04c0111c71bc8145508a8
mka <mka@A22PC5>
2023-09-11 20:59:45 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void GenerateArray(object obj)
+        {
+            var a = obj as Semaphore;
+
+            bool stop = false;
+            while (!stop)
+            {
+                if (a.WaitOne())
+                {
+                    try
+                    {
+                        int[] arr = new int[new Random().Next(1, 20)];
+                        for (int i = 0; i < arr.Length; i++) { arr[i] = new Random().Next(1, 100); }
+                        string tmp = $"Thread {Thread.CurrentThread.ManagedThreadId} generated the next list of number : ";
+                        for(int i = 0; i < arr.Length; i++) { tmp += ($" {arr[i]}"); }
+                        Console.WriteLine(tmp);
+                        Thread.Sleep(4000);
+                    }
+                    finally
+                    {
+                        stop = true;
+                        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} remove a lock");
+                        a.Release();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Timeout for thread {Thread.CurrentThread.ManagedThreadId} expired. Re-waiting...");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Semaphore s = new Semaphore(3, 3);
+            for(int i = 0; i < 15; ++i)
+            {
+                ThreadPool.QueueUserWorkItem(GenerateArray, s);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+using System.Xml;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static string path = "C:\\Users\\mka.STEP.000\\source\\repos\\ConsoleApp2";
+        public static string path2 = "C:\\Users\\mka.STEP.000\\source\\repos\\ConsoleApp1\\sum";
+        public static string path3 = "C:\\Users\\mka.STEP.000\\source\\repos\\ConsoleApp1\\mult";
+
+        static void Main(string[] args)
+        {
+            AutoResetEvent are = new AutoResetEvent(true); // initialState - is signaled state (free), false by default
+
+            ThreadPool.QueueUserWorkItem(GenerateAPair, are);
+
+            AutoResetEvent resetEvent = new AutoResetEvent(false);
+
+            ThreadPool.QueueUserWorkItem(Sum, resetEvent);
+            ThreadPool.QueueUserWorkItem(Mult, resetEvent);
+
+            Console.ReadKey();
+
+        }
+
+        public static void GenerateAPair(object obj)
+        {
+            var ev = obj as EventWaitHandle;
+
+            if (ev.WaitOne(1))
+            {
+
+                var tmp = (AutoResetEvent)obj;
+                ev.Reset();
+                KeyValuePair<int, int>[] kvp = new KeyValuePair<int, int>[5];
+                for (int i = 0; i < kvp.Length; i++)
+                {
+                    kvp[i] = new KeyValuePair<int, int>(new Random().Next(1, 20), new Random().Next(1, 20));
+                }
+                string jsonString = JsonConvert.SerializeObject(kvp, Newtonsoft.Json.Formatting.None);
+                File.WriteAllText(path, jsonString);
+                Console.WriteLine(jsonString);
+            }
+            else
+            {
+                Console.WriteLine("Thread {0} late", Thread.CurrentThread.ManagedThreadId);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void Sum(object obj)
+        {
+
+            var arr = JsonConvert.DeserializeObject<KeyValuePair<int, int>[]>(File.ReadAllText(path));
+
+            int[] sumarr = new int[arr.Length];
+
+            for(int i = 0; i < arr.Length; i++) {sumarr[i] = arr[i].Value + arr[i].Key;}
+
+            File.WriteAllText(path2, JsonConvert.SerializeObject(sumarr, Newtonsoft.Json.Formatting.None));
+            
+            (obj as EventWaitHandle).WaitOne();
+        }
+
+        public static void Mult(object obj)
+        {
+            
+
+            var arr = JsonConvert.DeserializeObject<KeyValuePair<int, int>[]>(File.ReadAllText(path));
+
+            int[] multiarr = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++) { multiarr[i] = arr[i].Value * arr[i].Key; }
+
+            File.WriteAllText(path3, JsonConvert.SerializeObject(multiarr, Newtonsoft.Json.Formatting.None));
+
+            (obj as EventWaitHandle).WaitOne();
+        }
+
+
+    }
+}
